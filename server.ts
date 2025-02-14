d97922bdf12af3b8403188f92884c5d1c604d8be
Charlie Puente <puentematos@gmail.com>
2025-02-14 18:43:23 -0500

    Added POST request and tested functionality

--- server.ts
+++ server.ts
@@ -1,5 +1,6 @@
 import express from "express"
 import { Request, Response } from "express";
+import fs from "fs"
 
 const PORT = 8080
 
@@ -12,6 +13,24 @@ app.get("/api/getMePizza", (req: Request, res: Response) => {
     res.json({
         message: "This route gets you pizza in 20 minutes or it's free."
     })
+})
+
+app.post("/api/createOrder", (req: Request, res: Response) => {
+
+    interface pizzaOrder {
+        size: string,
+        number: number,
+        toppings: string[]
+    }
+
+    let data: pizzaOrder = req.body
+
+    fs.writeFile("orders.txt", JSON.stringify(data), function() {
+        res.status(200).json({
+            message: `Pizza order received!`
+        })
+    })
+
 })
 
 
